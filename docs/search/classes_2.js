f14469b003478bcaa28cb07ffb6314226fa778ec
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-08-04 21:20:48 +0000

    Deploying to gh-pages from @ tatami-inc/tatami_chunked@e94c1873623386f7d16bd12155edb999712ffbc5 🚀

--- docs/search/classes_2.js
+++ docs/search/classes_2.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['cachedslab_0',['CachedSlab',['../structtatami__chunked_1_1SubsettedOracleSlabCache_1_1CachedSlab.html',1,'tatami_chunked::SubsettedOracleSlabCache']]],
-  ['compressedsparsematrix_1',['CompressedSparseMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1CompressedSparseMatrix.html',1,'tatami']]],
-  ['consecutiveoracle_2',['ConsecutiveOracle',['https://tatami-inc.github.io/tatami/structtatami_1_1ConsecutiveOracle.html',1,'tatami']]],
-  ['customchunkeddensematrix_3',['CustomChunkedDenseMatrix',['../classtatami__chunked_1_1CustomChunkedDenseMatrix.html',1,'tatami_chunked']]],
-  ['customchunkedoptions_4',['CustomChunkedOptions',['../structtatami__chunked_1_1CustomChunkedOptions.html',1,'tatami_chunked']]],
-  ['customchunkedsparsematrix_5',['CustomChunkedSparseMatrix',['../classtatami__chunked_1_1CustomChunkedSparseMatrix.html',1,'tatami_chunked']]]
+  ['compressedsparsecontents_1',['CompressedSparseContents',['https://tatami-inc.github.io/tatami/structtatami_1_1CompressedSparseContents.html',1,'tatami']]],
+  ['compressedsparsematrix_2',['CompressedSparseMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1CompressedSparseMatrix.html',1,'tatami']]],
+  ['consecutiveoracle_3',['ConsecutiveOracle',['https://tatami-inc.github.io/tatami/structtatami_1_1ConsecutiveOracle.html',1,'tatami']]],
+  ['customchunkeddensematrix_4',['CustomChunkedDenseMatrix',['../classtatami__chunked_1_1CustomChunkedDenseMatrix.html',1,'tatami_chunked']]],
+  ['customchunkedoptions_5',['CustomChunkedOptions',['../structtatami__chunked_1_1CustomChunkedOptions.html',1,'tatami_chunked']]],
+  ['customchunkedsparsematrix_6',['CustomChunkedSparseMatrix',['../classtatami__chunked_1_1CustomChunkedSparseMatrix.html',1,'tatami_chunked']]]
 ];
--- docs/search/variables_8.js
+++ docs/search/variables_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['primary_5flength_0',['primary_length',['../structtatami__chunked_1_1TypicalSlabCacheWorkspace.html#aa1f492cff84927346fe0c2da318f1e0c',1,'tatami_chunked::TypicalSlabCacheWorkspace']]]
+  ['pointers_0',['pointers',['https://tatami-inc.github.io/tatami/structtatami_1_1CompressedSparseContents.html#ad56fc74d757e9dcd8ffbdc48d5adc692',1,'tatami::CompressedSparseContents']]],
+  ['primary_5flength_1',['primary_length',['../structtatami__chunked_1_1TypicalSlabCacheWorkspace.html#aa1f492cff84927346fe0c2da318f1e0c',1,'tatami_chunked::TypicalSlabCacheWorkspace']]]
 ];
