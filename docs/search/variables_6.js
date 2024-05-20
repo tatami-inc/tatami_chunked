c3461d29bee29a5d2378f058de3b1624c9643bea
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-05-20 21:36:01 +0000

    Deploying to gh-pages from @ tatami-inc/tatami_chunked@5124de1ff4b10d2da4b0bbd18025fccf7eb3b62d 🚀

--- docs/search/variables_6.js
+++ docs/search/variables_6.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['non_5fzero_5fdepends_5fon_5fcolumn_0',['non_zero_depends_on_column',['https://tatami-inc.github.io/tatami/classtatami_1_1DelayedUnaryIsometricMockAdvanced.html#a87b2ce05b1929f85ee1ae629511ede5e',1,'tatami::DelayedUnaryIsometricMockAdvanced']]],
-  ['non_5fzero_5fdepends_5fon_5frow_1',['non_zero_depends_on_row',['https://tatami-inc.github.io/tatami/classtatami_1_1DelayedUnaryIsometricMockAdvanced.html#a20f3d96ca169707b18519854f9c83a7e',1,'tatami::DelayedUnaryIsometricMockAdvanced']]],
-  ['num_5fchunks_2',['num_chunks',['../structtatami__chunked_1_1ChunkDimensionStats.html#a9dab0db60e6bc39edd3bc643c054998b',1,'tatami_chunked::ChunkDimensionStats']]],
-  ['number_3',['number',['https://tatami-inc.github.io/tatami/structtatami_1_1SparseRange.html#a8b959e01323bdbe112a6432b3c51a038',1,'tatami::SparseRange::number'],['../structtatami__chunked_1_1SparseSlabFactory_1_1Slab.html#ac203879452a328acabf08db650737199',1,'tatami_chunked::SparseSlabFactory::Slab::number']]]
+  ['num_5fchunks_0',['num_chunks',['../structtatami__chunked_1_1ChunkDimensionStats.html#a9dab0db60e6bc39edd3bc643c054998b',1,'tatami_chunked::ChunkDimensionStats']]],
+  ['number_1',['number',['https://tatami-inc.github.io/tatami/structtatami_1_1SparseRange.html#a8b959e01323bdbe112a6432b3c51a038',1,'tatami::SparseRange::number'],['../structtatami__chunked_1_1SparseSlabFactory_1_1Slab.html#ac203879452a328acabf08db650737199',1,'tatami_chunked::SparseSlabFactory::Slab::number']]]
 ];
